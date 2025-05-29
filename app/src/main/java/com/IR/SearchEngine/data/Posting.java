52b0655084905fc833719872413cf1ccda782ae0
FeliksMindoTuaSitorus <felikssitorus@gmail.com>
2025-05-29 19:41:56 +0700

    feat: add posting and inverted index

--- app/src/main/java/com/IR/SearchEngine/data/Posting.java
+++ app/src/main/java/com/IR/SearchEngine/data/Posting.java
@@ -4,6 +4,8 @@
  */
 package com.IR.SearchEngine.data;
 
+import java.util.List;
+
 /**
  * Represents a posting in the inverted index.
  * A posting contains information about a term's occurrence in a specific document.
@@ -18,8 +20,45 @@ package com.IR.SearchEngine.data;
  * - Should be optimized for memory efficiency
  * - May contain additional information like term positions for phrase queries
  * 
- * @author alexhere
+ * @author alexhere updated by feliks
  */
 public class Posting {
+
+    /**
+     * Constructs a Posting object with the specified parameters.
+     * 
+     * @author feliks
+     * 
+     * @param docId The document ID where the term occurs
+     * @param termFrequency The frequency of the term in the document
+     * @param positions The positions of the term in the document
+     * @return A new Posting object
+     */
     
+    private final int docId; 
+    private int termFrequency; 
+    private List<Integer> positions;
+    
+    public Posting(int docId, int termFrequency, List<Integer> positions) {
+        this.docId = docId;
+        this.termFrequency = termFrequency;
+        this.positions = positions;
+    }
+
+    public int getDocId() {
+        return docId;
+    }
+
+    public int getTermFrequency() {
+        return termFrequency;
+    }
+
+    public List<Integer> getPositions() {
+        return positions;
+    } 
+
+    public void incrementFrequency() {
+        this.termFrequency++;
+    }
+
 }
